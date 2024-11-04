0c0bc998387d63d9f62dc73258f52d34b1ebf336
vipinvsist <vashistvipin42@gmail.com>
2024-11-04 23:23:39 +0530

    Data Transformation

--- src/mlflowProject/components/data_validation.py
+++ src/mlflowProject/components/data_validation.py
@@ -12,9 +12,6 @@ class DataValidation:
             validation_stauts = None
 
             data = pd.read_csv(self.config.unzip_data_dir)
-            data['Weight'].fillna(data['Weight'].mean(),inplace=True)
-            data['OutletSize'].fillna(data['OutletSize'].mode()[0],inplace=True)
-            data.drop(["ProductID", "OutletID"], axis=1, inplace=True)
             all_cols = list(data.columns)
             all_schema = self.config.all_schema.keys()
 
